9c68d21e43ef2b918482116efb0f7188dccf32be
magusnigri <link_nigri@hotmail.com>
2022-11-17 18:06:30 -0300

    recursos

---
+++ assets/script/recursos.js
@@ -0,0 +1,51 @@
+const listaDeRecursos = document.getElementById('lista__recursos');
+const addRecursoBtn = document.getElementById('recurso__btn--add');
+
+addRecursoBtn.addEventListener('click', () =>{
+        addRecurso();
+})
+
+function addRecurso(){
+    const recurso = document.createElement('li');
+    const recursoLabel = document.createElement('label');
+    const recursoTotal = document.createElement('input');
+    const recursoQuantidade = document.createElement('input');
+    const recursoNome = document.createElement('input');
+    const recursoBtn = document.createElement('button');
+
+    listaDeRecursos.appendChild(recurso);
+    recurso.classList.add('li__recurso');
+
+    recurso.appendChild(recursoLabel);
+    recursoLabel.classList.add('total__label');
+    recursoLabel.innerHTML = 'Total:';
+    
+    recursoLabel.appendChild(recursoTotal);
+    recursoTotal.classList.add('caixa');
+    recursoTotal.classList.add('caixa--recurso');
+    recursoTotal.classList.add('caixa--total');
+    recursoTotal.setAttribute('type','number');
+    recursoTotal.setAttribute('placeholder','0');
+
+    recurso.appendChild(recursoQuantidade);
+    recursoQuantidade.classList.add('caixa');
+    recursoQuantidade.classList.add('caixa--recurso');
+    recursoQuantidade.classList.add('caixa--quantidade');
+    recursoQuantidade.setAttribute('type','number');
+    recursoQuantidade.setAttribute('placeholder','0/0');
+
+    recurso.appendChild(recursoNome);
+    recursoNome.classList.add('title');
+    recursoNome.classList.add('title--sub');
+    recursoNome.classList.add('title--recurso__input');
+    recursoNome.setAttribute('type','text');
+    recursoNome.setAttribute('placeholder','Nome');
+
+    recurso.appendChild(recursoBtn);
+    recursoBtn.classList.add('btn--remove');
+    recursoBtn.classList.add('btn--remove--recurso');
+    recursoBtn.addEventListener('click', ()=>{
+        recurso.remove();
+    })
+
+}
