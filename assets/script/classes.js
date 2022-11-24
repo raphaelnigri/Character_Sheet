c83499b567236ce28c49169f43fcc148b4a560c0
magusnigri <link_nigri@hotmail.com>
2022-11-24 16:59:56 -0300

    custom class

--- assets/script/classes.js
+++ assets/script/classes.js
@@ -5,9 +5,14 @@ const listaDeClasses = document.getElementById('lista__classes');
 const lvlTotal = document.getElementById('char__lvl');
 const bonusDeProficiencia = document.getElementById('bonusDeProficiencia');
 
-addClassBtn.addEventListener('click', () =>{
+classLvl.addEventListener('input', ()=>{
+    if(classLvl.value > 20){
+        classLvl.value = 20;
+    }
+})
 
-    if(checaLimiteDeLvl(classLvl) && checaClasseRepetida(className.value) != 'repetido' && className.value != '' && classLvl.value != '' && classLvl.value <= 20){
+addClassBtn.addEventListener('click', () =>{
+    if(checaLimiteDeLvl(classLvl) && checaClasseRepetida(className.value) != 'repetido' && className.value != '' && classLvl.value != ''){
         addClass(className,classLvl);
         calculaLvlTotal();
         calculabonusDeProficiencia();
@@ -21,22 +26,34 @@ function addClass(nome,nivel){
     const label = document.createElement('label');
     const input = document.createElement('input');
     const del = document.createElement('button');
-    const arquetipo = document.createElement('input')
+    const arquetipo = document.createElement('input');
+    const custom = document.createElement('input');
 
     listaDeClasses.appendChild(novaClasse);
     novaClasse.setAttribute('data-classe',`${nome.value.toLowerCase()}`);
     novaClasse.classList.add('wrapper');
     
-    novaClasse.appendChild(label);
-    label.classList.add('class__nome');
-    label.setAttribute('for', `${nome.value.toLowerCase()}`);
-    label.innerHTML = `${nome.value.toLowerCase()}:`;
+    if(nome.value == 'custom'){
+        novaClasse.appendChild(custom);
+        custom.classList.add('class__nome');
+        custom.setAttribute('type','text');
+        custom.setAttribute('placeholder','Nome da Classe...');
+        custom.setAttribute('aria-label','Nome da Classe');
+    } else{
+        novaClasse.appendChild(label);
+        label.classList.add('class__nome');
+        label.setAttribute('for', `${nome.value.toLowerCase()}`);
+        label.innerHTML = `${nome.value.toLowerCase()}:`;
+    }
 
     novaClasse.appendChild(input);
     input.classList.add('class__input');
     input.classList.add('class__input--lvl');
     input.setAttribute('type','number');
-    input.setAttribute('id',`${nome.value.toLowerCase()}`);
+    input.setAttribute('data-inputLvl','');
+    if(nome.value != 'custom'){
+        input.setAttribute('id',`${nome.value.toLowerCase()}`);
+    }
     input.setAttribute('value',`${nivel.value}`);
     input.setAttribute('placeholder','lvl');
     input.setAttribute('aria-label','lvl');
@@ -75,7 +92,7 @@ function checaClasseRepetida(nome){
     var repetido = '';
     
     lista.forEach(element => {
-        if(element.dataset.classe == nome){
+        if(element.dataset.classe == nome && element.dataset.classe != 'custom'){
             repetido = 'repetido';
         }
     })
@@ -87,7 +104,7 @@ function calculaLvlTotal(){
     const lista = document.querySelectorAll('[data-classe]');
     
     lista.forEach(element => {
-        let nivel = element.querySelector('input').value;
+        let nivel = element.querySelector('[data-inputLvl]').value;
         nivelDeClasses += parseInt(nivel);
     })
 
